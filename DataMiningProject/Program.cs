8bc4191f638008a5077de70c4ef0f32839de4143
Christina Hinton <chrmhint@ut.utm.edu>
2019-02-08 19:24:05 -0600

    added HTML remover

--- DataMiningProject/Program.cs
+++ DataMiningProject/Program.cs
@@ -1,12 +1,42 @@
-﻿using System;
+﻿//TODO: capability to loop over 10k times to remove all tags from every document in the set
 
-namespace DataMiningProject
+using System;
+using System.IO;
+using System.Text;
+using HtmlAgilityPack;
+
+namespace DataMining_Project1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //HTML document to read
+            var htmlDoc = new HtmlDocument();
+
+            //get all text from file
+            string text = System.IO.File.ReadAllText("assignment1.html");
+
+            //load text in context of HTML
+            htmlDoc.LoadHtml(text);
+
+            //find all instances of the given tag type -> <html> because it surrounds every other tag
+            var htmlNodes = htmlDoc.DocumentNode.SelectNodes("//html");
+
+            //output file
+            StreamWriter outFile = new StreamWriter("test.txt");
+
+            //write all inner text to the output file
+            foreach (var node in htmlNodes)
+            {
+                outFile.WriteLine(node.InnerText);
+
+            }
+
+            //let the user know conversion is complete
+            Console.WriteLine("Conversion complete.");
+            Console.ReadLine();
+
         }
     }
 }
